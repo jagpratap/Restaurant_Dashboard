df1b2401dcd13e7b1af818093ef4c50e7eea9bcb
jagpratap <jagpratapchauhan@gmail.com>
2022-10-23 19:20:58 +0530

    Add login page UI

--- src/components/system/auth/login/index.js
+++ src/components/system/auth/login/index.js
@@ -1,7 +1,26 @@
 const Login = () => (
-  <div>
-    Login
-  </div>
+  <main className="login_page">
+    <div className="page_card">
+      <img className="card_illustration" src="/assets/images/svg/secure_login.svg" alt="secure_login" />
+      <form className="card_form">
+        <input
+          type="text"
+          id="username"
+          name="username"
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+        />
+        <input
+          type="submit"
+          id="submit"
+          name="submit"
+        />
+      </form>
+    </div>
+  </main>
 );
 
 export default Login;
