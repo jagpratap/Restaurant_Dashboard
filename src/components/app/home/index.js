a54c891aeb370b945eb965ce599cc6e0ba5f0723
jagpratap <jagpratapchauhan@gmail.com>
2022-10-27 13:41:45 +0530

    Add home's search and add inputs

--- src/components/app/home/index.js
+++ src/components/app/home/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
 
-const Home = () => (
-  <div>Home</div>
-);
+const Home = () => {
+  const [isLoading, setLoading] = useState(false);
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <section className="home-section">
+      <div className="section-header">
+        <form className="header-form" onSubmit={onSubmitHandler}>
+          <input
+            type="search"
+            id="search"
+            name="search"
+            placeholder="Search"
+          />
+          <div className="submit_field">
+            <input
+              type="submit"
+              id="add"
+              name="add"
+              value="Add"
+            />
+            {isLoading && <div className="lds-dual-ring" />}
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
