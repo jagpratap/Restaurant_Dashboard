55820f75e5a5af184bfce3b532fcffe3e46a6e90
jagpratap <jagpratapchauhan@gmail.com>
2022-10-27 13:37:59 +0530

    Add visited link color scheme

--- src/components/layouts/protected/sidebar/index.js
+++ src/components/layouts/protected/sidebar/index.js
@@ -1,14 +1,32 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import { NavLink, useLocation } from "react-router-dom";
 
-const Sidebar = () => (
-  <section className="sidebar-section">
-    <nav className="section-nav">
-      <ul className="nav-list">
-        <li className="list-item">Home</li>
-        <li className="list-item">Bookmark</li>
-      </ul>
-    </nav>
-  </section>
-);
+const Sidebar = () => {
+  const location = useLocation();
+  return (
+    <section className="sidebar-section">
+      <nav className="section-nav">
+        <ul className="nav-list">
+          <li className="list-item">
+            <NavLink
+              to="/dashboard/home"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="list-item">
+            <NavLink
+              to="/dashboard/bookmarks"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Bookmarks
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
 
 export default Sidebar;
